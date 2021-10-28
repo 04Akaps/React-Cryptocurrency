0dae4d5283f1eaa5b2d81ffc4c7bbee036aa1f24
유호진 <83758624+HoJin-jjimgo@users.noreply.github.com>
2021-10-28 23:04:42 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+클론 코딩
+
+- https://www.youtube.com/watch?v=9DDX3US3kss
+
+RapidApi
+
+- API를 가져 오는 곳
+- https://rapidapi.com/hub
+
+사용한 모듈(설치한 모듈)
+
+- antd, redux, axios, chart.js, html-react-parser, millify, moment, dom, '@ant-design/icons'
+
+참고한 사이트
+
+- https://ant.design/ === 디자인 용도
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Spacd } from "antd";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
